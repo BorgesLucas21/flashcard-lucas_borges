83752571f37fb261d45cc739eefaba84276a9cd4
BorgesLucas21 <141366250+BorgesLucas21@users.noreply.github.com>
2024-11-18 11:11:36 -0300

    4

--- perguntas.js
+++ perguntas.js
@@ -45,3 +45,9 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
